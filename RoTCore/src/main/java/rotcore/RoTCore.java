50cd84fd725340bc51826239bc897fa382ca5ccb
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-04-20 22:08:31 +0200

    Add plugin.yml to define path to main class
    Signed-off-by: PublicGameCoder <mikedegroot1999@hotmail.com>

---
+++ RoTCore/src/main/java/rotcore/RoTCore.java
@@ -0,0 +1,18 @@
+package rotcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RoTCore extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Implement onEnable
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Implement onDisable
+		
+	}
+}
